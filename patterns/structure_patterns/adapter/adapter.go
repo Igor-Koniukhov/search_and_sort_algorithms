30b6a36a29de86d4f5211424838840098bc6a77a
Ihor Koniukhov <koniukhov@bubuntu-lx>
2024-03-20 18:41:25 +0200

    added adapter

--- patterns/structure_patterns/adapter/adapter.go
+++ patterns/structure_patterns/adapter/adapter.go
@@ -1,1 +1,46 @@
 package main
+
+import "fmt"
+
+type ModernPrinter interface {
+	PrintStored() string
+}
+
+type LegacyPrinter interface {
+	Print(s string) string
+}
+
+type MyLegacyPrinter struct{}
+
+func (p *MyLegacyPrinter) Print(s string) string {
+	result := "Legacy Printer: " + s
+	fmt.Println(result)
+	return result
+}
+
+type PrinterAdapter struct {
+	OldPrinter LegacyPrinter
+	Msg        string
+}
+
+func (p *PrinterAdapter) PrintStored() string {
+	if p.OldPrinter != nil {
+		return p.OldPrinter.Print("Adapter: " + p.Msg)
+	} else {
+		return p.Msg
+	}
+}
+
+func main() {
+	// Using the new system directly
+	modernPrinter := &PrinterAdapter{Msg: "Hello World"}
+	fmt.Println(modernPrinter.PrintStored())
+
+	// Adapting the old system
+	legacyPrinter := &MyLegacyPrinter{}
+	adapter := &PrinterAdapter{
+		OldPrinter: legacyPrinter,
+		Msg:        "Hello World",
+	}
+	fmt.Println(adapter.PrintStored())
+}
