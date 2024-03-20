251dcca81b62acd0bf58ef19a6dfa1bb69631577
Ihor Koniukhov <koniukhov@bubuntu-lx>
2024-03-20 18:31:22 +0200

    implemented prototype

--- patterns/creational_patterns/prototype/prototype.go
+++ patterns/creational_patterns/prototype/prototype.go
@@ -1,1 +1,26 @@
-package prototype
+package main
+
+import "fmt"
+
+type Prototype interface {
+	Clone() Prototype
+}
+
+type File struct {
+	Name    string
+	Content string
+}
+
+func (f *File) Clone() Prototype {
+	// Create a new File object and copy the values.
+	return &File{Name: f.Name + "_copy", Content: f.Content}
+}
+
+func main() {
+	originalFile := &File{Name: "original", Content: "This is the original file"}
+	clonedFile := originalFile.Clone()
+
+	fmt.Printf("Original: %#v\n", originalFile)
+	fmt.Printf("Cloned: %#v\n", clonedFile)
+	fmt.Printf("Cloned clone: %#v\n", clonedFile.Clone())
+}
